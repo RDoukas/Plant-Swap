45607b032fb8e794e084e92834837abf4abbe718
Becca <rrdoukas@gmail.com>
2020-08-25 11:23:05 -0500

    updates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
-
-# User.create!([
-#   {username: "SNix", password_digest: "$2a$12$ezYomPVL7ygDDaWVeJXgrezxyN.d3dONALp/MpY51BsrUekHXy6hG", first_name: "Steph", last_name: "Nixon", email: "snixon@gmail.com"},
-#   {username: "amyers", password_digest: "$2a$12$3vAJwyJCUL1eEeNsYGhimex2S2UJAAbvnBXLKWxrW4gy5pbP6mtJq", first_name: "Amy", last_name: "Myers", email: "amyers@gmail.com"},
-#   {username: "carl", password_digest: "$2a$12$mbB2qTlairNFv1nHavPwae6VE68XI08GTiZ5r./QOdjoX82TUko76", first_name: "Carl", last_name: "Hauck", email: "carl@gmail.com"},
-#   {username: "JakeSnake", password_digest: "$2a$12$jVRut4Zp.wTZKzRlp408/.kRWAn92dVIH8YY9yJdR3PUk/aU9djEq", first_name: "Jacob", last_name: "Doukas", email: "JakeSnake@gmail.com"},
-#   {username: "Betty", password_digest: "$2a$12$M9Bf4gySRSi1vRtsyfDopuLwqg.Lfe8Ij5lDUI.ZW0eXPWUQKQIM2", first_name: "Betty", last_name: "Doukas", email: "Betty@gmail.com"},
-#   {username: "alex", password_digest: "$2a$12$a13WO.jlr7zPrmWofrV6HeIh5RwBOaeVQDOl0a8Sc.Ms7ZCSALqaK", first_name: "Alex", last_name: "Rosales", email: "alex@gmail.com"},
-#   {username: "beccabagels", password_digest: "$2a$12$oKVZ4jlk3Gko8hfdpE/.CuWtnaqhNVWawkZmaD8lUYiZFnJCV9vnG", first_name: "Becca", last_name: "Doukas", email: "becca@gmail.com"}
-# ])
+User.create!([
+  {username: "SNix", password_digest: "$2a$12$6CnSuldoHIx8SJzv0wq.X.1fI4QjN.o3tlqKGEeasBAdHG9rOo/ba", first_name: "Steph", last_name: "Nixon", email: "snixon@gmail.com"},
+  {username: "amyers", password_digest: "$2a$12$896zYxNCnfOnd.xOrCquvexfqhjb6mX5ft4koxrpEldwovKqzYK2W", first_name: "Amy", last_name: "Myers", email: "amyers@gmail.com"},
+  {username: "carl", password_digest: "$2a$12$3MXsXMxr.ARuuPeul22gp.RJnytg3svKRCx31du4ohmwQPpYfYkny", first_name: "Carl", last_name: "Hauck", email: "carl@gmail.com"},
+  {username: "JakeSnake", password_digest: "$2a$12$ZibUmJktiZo7iEuu/GOMV.0/Ae5v56eAr6n07nyvVB8S5tjei2pre", first_name: "Jacob", last_name: "Doukas", email: "JakeSnake@gmail.com"},
+  {username: "Betty", password_digest: "$2a$12$h9RFaLgxELPXlAqOdF8hq.EVg7m2B7fMupKIbQr4JwmSd266te3JS", first_name: "Betty", last_name: "Doukas", email: "Betty@gmail.com"},
+  {username: "alex", password_digest: "$2a$12$yZYalgA90KPNd9KwLEGopuz1b3kHumwdBLwDYeBRSpxkjUAwGaSKi", first_name: "Alex", last_name: "Rosales", email: "alex@gmail.com"},
+  {username: "beccabagels", password_digest: "$2a$12$nTZySmGE3.eePSxVvE6.PeS3PQyCexVlkTLUlP.3J6zdxjU0sbw9i", first_name: "Becca", last_name: "Doukas", email: "becca@gmail.com"}
+])
 
 Ad.create!([
   {title: "Burritto Succulent", user_id: 5, description: " I have an adorable burrito succulent potted in a 3 inch ceramic pot. These guys grow like crazy and will trail over the pot. They're great to hang!  Let me know if you have any questions. ", image_url: "https://res.cloudinary.com/dzx2kuqwt/image/upload/v1596668993/f3xcsqxwtjndawz4qowb.jpg"},
@@ -44,7 +43,6 @@ Category.create!([
   {name: "Seeds"},
   {name: "Potted"}
 ])
-
 AdCategory.create!([
   {category_id: 2, ad_id: 4},
   {category_id: 3, ad_id: 4},
@@ -52,12 +50,6 @@ AdCategory.create!([
   {category_id: 13, ad_id: 4},
   {category_id: 20, ad_id: 4},
   {category_id: 8, ad_id: 4},
-  {category_id: 1, ad_id: 11},
-  {category_id: 3, ad_id: 11},
-  {category_id: 6, ad_id: 11},
-  {category_id: 8, ad_id: 11},
-  {category_id: 20, ad_id: 11},
-  {category_id: 13, ad_id: 11},
   {category_id: 15, ad_id: 1},
   {category_id: 2, ad_id: 1},
   {category_id: 4, ad_id: 1},
@@ -69,11 +61,6 @@ AdCategory.create!([
   {category_id: 6, ad_id: 3},
   {category_id: 10, ad_id: 3},
   {category_id: 18, ad_id: 3},
-  {category_id: 1, ad_id: 10},
-  {category_id: 3, ad_id: 10},
-  {category_id: 6, ad_id: 10},
-  {category_id: 13, ad_id: 10},
-  {category_id: 20, ad_id: 10},
   {category_id: 2, ad_id: 7},
   {category_id: 3, ad_id: 7},
   {category_id: 6, ad_id: 7},
@@ -99,14 +86,7 @@ AdCategory.create!([
   {category_id: 2, ad_id: 5},
   {category_id: 7, ad_id: 5},
   {category_id: 18, ad_id: 5},
-  {category_id: 11, ad_id: 5},
-  {category_id: 1, ad_id: 12},
-  {category_id: 4, ad_id: 12},
-  {category_id: 6, ad_id: 12},
-  {category_id: 2, ad_id: 13},
-  {category_id: 4, ad_id: 14},
-  {category_id: 1, ad_id: 15},
-  {category_id: 6, ad_id: 15}
+  {category_id: 11, ad_id: 5}
 ])
 
 Conversation.create!([
@@ -114,11 +94,8 @@ Conversation.create!([
   {sender_id: 3, recipient_id: 1, ad_id: 1},
   {sender_id: 1, recipient_id: 5, ad_id: 4},
   {sender_id: 2, recipient_id: 3, ad_id: 3},
-  {sender_id: 6, recipient_id: 3, ad_id: 10},
   {sender_id: 3, recipient_id: 5, ad_id: 4},
   {sender_id: 3, recipient_id: 2, ad_id: 2},
-  {sender_id: 5, recipient_id: 3, ad_id: 10},
-  {sender_id: 6, recipient_id: 5, ad_id: 11},
   {sender_id: 3, recipient_id: 2, ad_id: 2}
 ])
 Message.create!([
@@ -136,40 +113,8 @@ Message.create!([
   {body: " Thank you!! Would you like to adopt it?", user_id: 1, conversation_id: 4},
   {body: " This succulent is so cute! I've never seen one like it! What part of town are you located? I see you would like it picked up. ", user_id: 1, conversation_id: 5},
   {body: " I've always wanted a swiss cheese plant and have been dying to try propagation!! When can you drop it off?", user_id: 1, conversation_id: 6},
-  {body: " I love this plant!!! Is it still available? ", user_id: 2, conversation_id: 8},
-  {body: " This is so cute and I love the name!! I'm new to the plant mom love and hoping to find something easy but cute! What's your schedule like this week?", user_id: 6, conversation_id: 9},
-  {body: " This is so cute! When are you free this week?", user_id: 3, conversation_id: 11},
-  {body: " Hello I love your plant! ", user_id: 2, conversation_id: 12},
-  {body: " Yes it is! It started as a propagation but is starting to root nicely! ", user_id: 3, conversation_id: 8},
-  {body: " Awesome! Are you free this week for the swap?", user_id: 2, conversation_id: 8},
-  {body: " I'm free every day after 5 pm. Is there a day that works best for you? ", user_id: 3, conversation_id: 9},
-  {body: " This plant is so interesting and hearty, I've never seen anything like it! Are you free this week? I'm in Lakeview ", user_id: 3, conversation_id: 13},
-  {body: " I am, I'm in Buena Park so right there!! How does Wednesday sound??", user_id: 3, conversation_id: 13},
-  {body: " Yes it does! I'll see you then! ", user_id: 3, conversation_id: 13},
-  {body: " This looks so cool! Are you free this week?", user_id: 3, conversation_id: 14},
-  {body: " Yes I am! How about Tuesday at 6 pm?", user_id: 2, conversation_id: 14},
-  {body: " Fake", user_id: 2, conversation_id: 15},
   {body: " Yes I would! I've always wanted an orchid! Are you free this week? ", user_id: 3, conversation_id: 4},
-  {body: " Yes I am! Tuesday after 5 pm works for me! Is that good for you?", user_id: 3, conversation_id: 8},
   {body: " Yes I am! How does Tuesday sound? I'm around all day!", user_id: 1, conversation_id: 4},
-  {body: " Yes I would. I'm in the loop. Does that work for you? ", user_id: 5, conversation_id: 5},
-  {body: " I'm free on Tuesday and Thursday all day. Which day works best for you?", user_id: 5, conversation_id: 11},
-  {body: " Is the pincushion still available? I'm looking for something easy to grow as I'm new to this and love the interesting shape. ", user_id: 5, conversation_id: 16},
-  {body: " How about Thursday at 6 pm?", user_id: 6, conversation_id: 9},
-  {body: " This plant is so cool! Are you free this week to drop it off?", user_id: 6, conversation_id: 17},
-  {body: " Perfect! I'll stop by around 12 pm.", user_id: 3, conversation_id: 4},
-  {body: " Perfect! I'll see you then!", user_id: 3, conversation_id: 9},
-  {body: " Perfect!!!", user_id: 3, conversation_id: 14},
-  {body: " How about Thursday at 3 pm?", user_id: 3, conversation_id: 11},
-  {body: " Yes it is! It's super easy to grow and so cool! It's great for propagation too! I'm busy this week but how about next week?", user_id: 3, conversation_id: 16},
-  {body: " That's great! I'll see you then! ", user_id: 5, conversation_id: 11},
-  {body: " That works for me! How about next Monday evening?", user_id: 5, conversation_id: 16},
-  {body: " Yes I am! What part of town are you in?", user_id: 5, conversation_id: 17},
-  {body: " Great! ", user_id: 6, conversation_id: 9},
-  {body: " I'm in the South Loop. Does noon on Thursday okay?", user_id: 6, conversation_id: 17},
-  {body: " Yes it is! See you then!!! ", user_id: 2, conversation_id: 8},
-  {body: " Awesome!!!!", user_id: 3, conversation_id: 8},
-  {body: " Hi! I love your plant! \n", user_id: 3, conversation_id: 21},
-  {body: " Thanks so much!", user_id: 2, conversation_id: 21}
+  {body: " Yes I would. I'm in the loop. Does that work for you? ", user_id: 5, conversation_id: 5}
 ])
 
