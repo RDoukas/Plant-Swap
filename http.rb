b798b769519163fa5bd81ac0187c8ed7eb028792
Becca <rrdoukas@gmail.com>
2020-08-27 14:35:47 -0500

    updates

--- http.rb
+++ http.rb
@@ -1,11 +1,11 @@
 require "http"
 
-response = HTTP.get("https://chi-plant-swap.herokuapp.com/api/")
+response = HTTP.get("https://chi-plant-swap.herokuapp.com/api/ads")
 
 puts response
 
 require "http"
 
-response = HTTP.get("https://chi-plant-swap.herokuapp.com/api/")
+response = HTTP.get("https://chi-plant-swap.herokuapp.com/api/ads")
 
 puts response
