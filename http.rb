a4b5fa6a192adf6adb4c9d3ad9603d5b5df955e1
Becca <rrdoukas@gmail.com>
2020-08-27 14:33:53 -0500

    adds http file

---
+++ http.rb
@@ -0,0 +1,11 @@
+require "http"
+
+response = HTTP.get("https://chi-plant-swap.herokuapp.com/api/")
+
+puts response
+
+require "http"
+
+response = HTTP.get("https://chi-plant-swap.herokuapp.com/api/")
+
+puts response
