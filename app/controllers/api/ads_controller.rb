11cd5cb882e23ad864705f22d7422d41a8cd14b2
Becca <rrdoukas@gmail.com>
2020-08-28 13:45:28 -0500

    updates files

--- app/controllers/api/ads_controller.rb
+++ app/controllers/api/ads_controller.rb
@@ -2,81 +2,82 @@ class Api::AdsController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
 
   def index 
-  @ads = Ad.all
-  render 'index.json.jb'
-  
+    @ads = Ad.all
+    # if params[:category]
+    #   category = Category.find_by(name: params[:category])
+    #   @ads = category.ads
+    # end
+    # if params[:search]
+    #   @ads = @ads.where("title iLIKE ?", "%#{params[:search]}%")
+    # end
+    # if params[:sort] == "title"
+    #   if params[:sort_order] == "asc"
+    #     @ads = @ads.order(:title)
+    #   elsif params[:sort_order] == "desc"
+    #     @ads = @ads.order(title: :desc)
+    #   end
+    # elsif params[:sort] == "category"
+    #   if params[:sort_order] == "asc"
+    #     @ads = @ads.order(:category)
+    #   elsif params[:sort_order] == "desc"
+    #     @ads = @ads.order(category: :desc)
+    #   end
+    # else 
+    #   @ads = @ads.order(:created_at)
+    # end
+    render 'index.json.jb'
+    
   end
 
   def show 
-  @ad = Ad.find_by(id: params[:id])
-  render "show.json.jb"
+    @ad = Ad.find_by(id: params[:id])
+    render "show.json.jb"
   end 
 
   def create
-  response = Cloudinary::Uploader.upload(params[:image_url])
-  cloudinary_url = response["secure_url"]
-  @ad = Ad.new(
-    title: params[:title],
-    user_id: current_user.id,
-    description: params[:description],
-    image_url: cloudinary_url
-  )
-
-  if @ad.save
-    eval(params[:category_ids]).each do |category_id|
-    AdCategory.create(
-      ad_id: @ad.id,
-      category_id: category_id,
+    response = Cloudinary::Uploader.upload(params[:image_url])
+    cloudinary_url = response["secure_url"]
+    @ad = Ad.new(
+      title: params[:title],
+      user_id: current_user.id,
+      description: params[:description],
+      # image_url: params[:image_url],
+      image_url: cloudinary_url
     )
+    if @ad.save
+      render "show.json.jb"
+    else 
+      render json: {errors: @ad.errors.full_messages}, status: :unprocessable_entity
     end 
-    render "show.json.jb"
-  else
-    render json: {errors: @ads.errors.full_messages}, status: 422
-  end
   end
  
 
   def update
-   
+    @ad = Ad.find_by(id: params[:id])
 
-  
-  
-  @ad = Ad.find_by(id: params[:id])
+    if @ad.user_id == current_user.id
+      @ad.title = params[:title] || @ad.title
+      @ad.description = params[:description] || @ad.description
+      @ad.image_url = params[:image_url] || @ad.image_url
+    
 
-  if @ad.user_id == current_user.id
-    @ad.title = params[:title] || @ad.title
-    @ad.description = params[:description] || @ad.description
-
-    # if conditional for if there's an image. 
-    response = Cloudinary::Uploader.upload(params[:image_url])
-    cloudinary_url = response["secure_url"]
-    @ad.image_url = cloudinary_url || @ad.image_url
-    # end 
-  
-
-    if @ad.save
-    @ad.ad_categories.destroy_all
-    eval(params[:category_ids]).each do |category_id|
-      AdCategory.create(
-      ad_id: @ad.id,
-      category_id: category_id
-      )
+      if @ad.save
+        render "show.json.jb" 
+      else 
+        render json: { errors: @ad.errors.full_messages}, status: unprocessable_entity
+      end 
+    else   
+      render json: {message: "You are unable to update this ad."}
     end
-    render "show.json.jb" 
-    else 
-    render json: {errors: @ads.errors.full_messages}, status: 422
-    end 
-  end 
   end 
 
   def destroy 
-  @ad = Ad.find_by(id: params[:id])
-  if @ad.user_id  == current_user.id
-    @ad.destroy
-  
-    render json: {message: "Your ad has been removed."}
-  else 
-    render json: {message: "You are unable to delete this ad."}
-  end
+    @ad = Ad.find_by(id: params[:id])
+    if @ad.user_id  == current_user.id
+      @ad.destroy
+      render json: {message: "Your ad has been removed."}
+    else 
+      render json: {message: "You are unable to delete this ad."}
+    end
   end 
 end
